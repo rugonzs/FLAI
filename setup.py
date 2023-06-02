20141e5fd30441f48a6d30fa1ea4fdb3b2824885
Rugonzs <rubo.g@icloud.com>
2023-06-02 21:34:51 +0200

    math error and repo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ VERSION = '1.0.0' #Muy importante, deberéis ir cambiando la versión de vuestra
 PACKAGE_NAME = 'FLAI_CAUSAL' #Debe coincidir con el nombre de la carpeta 
 AUTHOR = 'Rubén González Sendino' #Modificar con vuestros datos
 AUTHOR_EMAIL = 'rubo.g@icloud.com' #Modificar con vuestros datos
-URL = 'https://github.com/rugonzs' #Modificar con vuestros datos
+URL = 'https://github.com/rugonzs/FLAI' #Modificar con vuestros datos
 
 LICENSE = 'Apache-2.0 license' #Tipo de licencia
 DESCRIPTION = 'Library to creat causal model and mitigate the bias.' #Descripción corta
@@ -17,7 +17,7 @@ LONG_DESC_TYPE = "text/markdown"
 
 #Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
 INSTALL_REQUIRES = [
-      'bnlearn','networkx','matplotlib','pgmpy','numpy', 'itertools','math','pandas','sklearn'
+      'bnlearn','networkx','matplotlib','pgmpy','numpy', 'itertools',,'pandas','sklearn'
       ]
 
 setup(
