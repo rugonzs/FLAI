76308a52e31e4adb9cb1c430506877527dad2943
Rugonzs <rubo.g@icloud.com>
2023-06-02 22:00:28 +0200

    change skleran

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.2' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
+VERSION = '1.0.3' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
 PACKAGE_NAME = 'FLAI_CAUSAL' #Debe coincidir con el nombre de la carpeta 
 AUTHOR = 'Rubén González Sendino' #Modificar con vuestros datos
 AUTHOR_EMAIL = 'rubo.g@icloud.com' #Modificar con vuestros datos
@@ -17,7 +17,7 @@ LONG_DESC_TYPE = "text/markdown"
 
 #Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
 INSTALL_REQUIRES = [
-      'bnlearn','networkx','matplotlib','pgmpy','numpy', 'pandas','sklearn'
+      'bnlearn','networkx','matplotlib','pgmpy','numpy', 'pandas','scikit-learny'
       ]
 
 setup(
